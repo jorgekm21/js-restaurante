da3eac253282c64c7de50898a542729ae1ef5525
Jorge Kassabji <jorgekm21@gmail.com>
2021-07-14 16:49:23 -0400

    Funcionalidad mostrarMenu

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Detalles de repositorio:
+
+Proyecto final del curso de javascript desde cero con ED-Team.
+
+Esto forma parte de 1 de 2 ejercicios aplicados al proyecto final con el fin de obtener el certificado de finalizacion del curso.
--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hola Mundo')
+console.log('------- WELCOME TO THE ENGINEERING OF COOK -------')
+const productos = [{
+    codigo: "H100",
+    producto: "Hamburguesa simple",
+    costo: 20
+},{
+    codigo: "H200",
+    producto: "Hamburguesa doble",
+    costo: 30
+},{
+    codigo: "P100",
+    producto: "Papas Fritas",
+    costo: 8
+},{
+    codigo: "R100",
+    producto: "Refresco personal",
+    costo: 6
+}]
+
+const mostrarMenu = () => {
+    console.log("--------------- MENU -----------------")
+    return console.table(productos)
+}
